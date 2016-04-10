78fe644c6598e39d5b5b22faa93d502e8085ade2
Toshiki Teramura <toshiki.teramura@gmail.com>
2016-04-10 11:32:30 +0900

    Add error handling

--- rplugin/python3/toggl.py
+++ rplugin/python3/toggl.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import neovim
+import logging
 
 
 @neovim.plugin
@@ -8,7 +9,10 @@ class Toggl(object):
 
     def __init__(self, nvim):
         self.nvim = nvim
-        self.api_token = nvim.eval("g:toggl_api_token")
+        try:
+            self.api_token = nvim.eval("g:toggl_api_token")
+        except neovim.api.nvim.NvimError:
+            logging.error("API Token cannot be read")
 
     @neovim.function("TogglAPIToken", sync=True)
     def get_api_token(self, args):
